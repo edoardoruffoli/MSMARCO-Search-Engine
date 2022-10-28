535af8f5abcc4d194d04ceb5ee487bb6b717a30f
edoardoruffoli <ruffoli99@gmail.com>
2022-10-28 15:07:26 +0200

    Fixed issues

---
+++ include/MSMARCO-Search-Engine/inverted_index.hpp
@@ -0,0 +1,31 @@
+﻿// MSMARCO-Search-Engine.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+#include <cmath>
+#include <fstream>
+#include <random>
+#include <string>
+#include <unordered_set>
+#include <unordered_map>
+#include <sstream>
+#include <filesystem>
+
+// TODO: Reference additional headers your program requires here.
+
+// MSMARCO-Search-Engine.cpp : Defines the entry point for the application.
+//
+
+#include "MSMARCO-Search-Engine/inverted_index.hpp"
+
+std::string clearToken(const std::string& token);
+
+std::unordered_map<std::string, int> getTokens(const std::string &content, const int& doc_id);
+
+void invert_index(const std::unordered_map<std::string, int>& token_stream, std::unordered_map<std::string, std::list<int>>& dictionary);
+
+void write_block_to_disk(std::unordered_map<std::string, std::list<int>>& dictionary, int block_num);
+
+int getFileSize(const std::string& text);
--- src/inverted_index.cpp
+++ src/inverted_index.cpp
@@ -1,7 +1,7 @@
 ﻿// MSMARCO-Search-Engine.cpp : Defines the entry point for the application.
 //
 
-#include "MSMARCO-Search-Engine/inverted_index.h"
+#include "MSMARCO-Search-Engine/inverted_index.hpp"
 
 std::string clearToken(const std::string& token) {
 	std::string cleared_token;
@@ -61,53 +61,4 @@ int getFileSize(const std::string& text) {
 		}
 	}
 	return counter;
-}
-
-int main(int argc, char* argv[])
-{
-	const int BLOCK_SIZE = 10000000;
-	int current_size = 0;
-	int block_num = 1;
-
-	const char* docfile = argv[1];
-	//std::cout << "--->Parameters<---" << std::endl;
-	std::cout << "-> docfile: " << docfile << std::endl << std::endl;
-
-	std::ifstream f(docfile);
-
-	std::string loaded_content;
-	int pid;
-	std::string id;
-	std::string text;
-	std::unordered_map<std::string, std::list<int>> dictionary;
-
-	while (getline(f, loaded_content)) {
-		std::istringstream iss(loaded_content);
-		getline(iss, id, '\t');
-		pid = stoi(id);
-		getline(iss, text, '\t');
-		current_size += getFileSize(text);
-		if (current_size < BLOCK_SIZE) {
-			std::unordered_map<std::string, int> tokens = getTokens(text, pid);
-			invert_index(tokens, dictionary);
-		}
-		else {
-			write_block_to_disk(dictionary, block_num);
-			current_size = 0;
-			block_num++;
-			dictionary.clear();
-		}
-	}
-
-	write_block_to_disk(dictionary, block_num);
-
-	for (auto& s : dictionary)
-	{
-		std::cout << s.first << " ";
-		for (auto innerItr : s.second)
-			std::cout << innerItr << " ";
-		std::cout << std::endl;
-	}
-
-	f.close();
 }

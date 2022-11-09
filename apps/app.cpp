ec80dfd040d4b3db08a2d211636f222274808ea9
mrfransis <48528161+MrFransis@users.noreply.github.com>
2022-11-09 17:34:57 +0100

    add simple9 example

--- apps/app.cpp
+++ apps/app.cpp
@@ -1,12 +1,58 @@
 #include "MSMARCO-Search-Engine/io.hpp"
-#include "MSMARCO-Search-Engine/compressing.hpp"
+//#include "MSMARCO-Search-Engine/compressing.hpp"
 //#include "MSMARCO-Search-Engine/query.hpp"
+#include "MSMARCO-Search-Engine/simple9.hpp"
+
 
 int main(int argc, char* argv[]) {
     std::cout << "Reading" << std::endl;
     std::map<std::string, std::pair<unsigned long, size_t>> lexicon;
     //std::set<doc_entry> doc_table;
     load_lexicon(&lexicon, std::string("../../output/lexicon.bin"));
+
+    //************
+    //test simple9
+    //************
+
+    unsigned int result;
+    std::vector<unsigned int> encodeTest0 = { 3, 4, 6, 8, 9, 10, 12};
+    std::ofstream out_file("../tmp/simple9.bin", std::ios::binary);
+    int len;
+    len = writeS9(&result, encodeTest0, out_file);
+    unsigned int val[100] = { 0 };
+    len = readS9(&result, val);
+    for (int i = 0; i < len; i++) {
+        std::cout << val[i] << " ";
+    }
+    //if (result == 0x6fffffff)
+    //    std::cout << "encode successfully Len : " << len << std::endl;
+    //else
+    //    std::cout << "encode error" << std::endl;
+    /*
+    unsigned int val[100] = { 0 };
+    len = readS9(&result, val);
+    for (int i = 0; i < len; i++) {
+        std::cout << val[i] << " ";
+    }
+    std::cout << std::endl;
+
+    unsigned int encodeTest1[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+    len = writeS9(&result, encodeTest1, 10);
+    std::cout << " write Len " << len << std::endl;
+    len = readS9(&result, val);
+    for (int i = 0; i < len; i++) {
+        std::cout << val[i] << " ";
+    }
+    std::cout << std::endl;
+    */
+    return 0;
+
+
+    //*******************
+    //test binary writing
+    //*******************
+
+
     //init_data_structures();
     //auto it = doc_table.find(13);
     //std::cout << it->first << " " << it->second.first << " " << it->second.second << std::endl;
@@ -15,7 +61,11 @@ int main(int argc, char* argv[]) {
         std::cout << it->first << " " << it->second.first << " " << it->second.second << std::endl;
     }*/
 
+
+    //*************
     //openList test
+    //*************
+
     /*
     std::string term = "applianc";
     posting_list ret;
---
+++ include/MSMARCO-Search-Engine/simple9.hpp
@@ -0,0 +1,48 @@
+#pragma once
+
+#include <fstream>
+#include <iostream>
+#include <bitset>
+#include <vector>
+#include <cmath>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <memory>
+#include <unordered_set>
+#include <unordered_map>
+#include <map>
+#include <sstream>
+
+#include "MSMARCO-Search-Engine/util.hpp"
+#include "MSMARCO-Search-Engine/io.hpp"
+
+static const int NUM_DATA_BITS = 28;
+static const int NUM_SELECTOR_BITS = 4;
+
+static const unsigned char NUM_1_28_BITS = 1;
+static const unsigned char NUM_2_14_BITS = 2;
+static const unsigned char NUM_3_9_9_10_BITS = 3;
+static const unsigned char NUM_4_7_BITS = 4;
+static const unsigned char NUM_5_5_5_6_6_6_BITS = 5;
+static const unsigned char NUM_7_4_BITS = 6;
+static const unsigned char NUM_9_3_3_3_3_3_3_3_3_4_BITS = 7;
+static const unsigned char NUM_14_2_BITS = 8;
+static const unsigned char NUM_28_1_BITS = 9;
+
+static const int BITS_28_MASK = (1 << 28) - 1;
+static const int BITS_14_MASK = (1 << 14) - 1;
+static const int BITS_10_MASK = (1 << 10) - 1;
+static const int BITS_9_MASK = (1 << 9) - 1;
+static const int BITS_7_MASK = (1 << 7) - 1;
+static const int BITS_6_MASK = (1 << 6) - 1;
+static const int BITS_5_MASK = (1 << 5) - 1;
+static const int BITS_4_MASK = (1 << 4) - 1;
+static const int BITS_3_MASK = (1 << 3) - 1;
+static const int BITS_2_MASK = (1 << 2) - 1;
+static const int BITS_1_MASK = (1 << 1) - 1;
+
+int writeS9(unsigned int* result, std::vector<unsigned int> from, std::ofstream& ofile);
+
+int readS9(unsigned int* from, unsigned int* val);

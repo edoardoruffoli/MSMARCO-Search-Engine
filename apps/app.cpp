ffa4d951ff318a2b68718c8a807c3f31c0ead994
edoardoruffoli <ruffoli99@gmail.com>
2022-12-04 18:46:55 +0100

    Removed in memory doc table flag

--- apps/app.cpp
+++ apps/app.cpp
@@ -50,7 +50,7 @@ static void merge(std::ostream& out) {
     while (boost::filesystem::exists("../tmp/intermediate_" + std::to_string(n_blocks + 1))) {
         n_blocks++;
     }
-    merge_blocks(n_blocks, true);
+    merge_blocks(n_blocks);
     loaded_data = false;
 }
 
